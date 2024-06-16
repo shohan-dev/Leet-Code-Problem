09a02809a069965022f2940588bbdfe41648d06c
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-06-16 22:01:43 +0600

    problem 1

---
+++ 1-100/_1 Two Sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+        return None
+
