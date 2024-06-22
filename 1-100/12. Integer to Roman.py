e891d9a2eb5b008a260f6186257da21b00e63ee2
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-06-22 23:07:24 +0600

    problem 12

--- 1-100/12. Integer to Roman.py
+++ 1-100/12. Integer to Roman.py
@@ -1,6 +1,5 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        
         num_map = {
             1: "I",
             5: "V",    4: "IV",
@@ -10,10 +9,8 @@ class Solution:
             500: "D",  400: "CD",
             1000: "M", 900: "CM",
         }
-        
-        
+    
         r = ''
-        
         
         for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
             
