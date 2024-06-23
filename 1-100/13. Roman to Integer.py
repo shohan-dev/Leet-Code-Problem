5ad416904fbaef66776cc87231fd1677fa61d26d
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-06-23 23:39:28 +0600

    better

--- 1-100/13. Roman to Integer.py
+++ 1-100/13. Roman to Integer.py
@@ -1,14 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_integer = {
+        romanValues = {
             'I': 1,
             'V': 5,
             'X': 10,
             'L': 50,
             'C': 100,
             'D': 500,
-            'M': 1000,
+            'M': 1000
         }
-        
-        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
-        return sum(map(lambda x: roman_to_integer[x], s))
+
+        result = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            currValue = romanValues[s[i]]
+
+            if i < len(s) - 1 and currValue < romanValues[s[i + 1]]:
+                result -= currValue
+            else:
+                result += currValue
+
+        return result
